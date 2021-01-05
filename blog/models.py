6257d3141d9928cc277e137d79f505f81ae1a73f
Максим Секачёв <cement-fan@ya.ru>
2021-01-05 10:16:44 +0300

    templates

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
 
 class PublishedManager(models.Manager):
@@ -13,13 +16,6 @@ class Post(models.Model):
         ('published', 'Published'),
     )
 
-    objects = models.Manager()  # Менеджер по умолчанию.
-    published = PublishedManager()  # Наш новый менеджер.
-    # С помощью следующей команды мы получим все опубликованные статьи,
-    # название которых начинается с Who:
-    # Post.published.filter(title__startswith='Who')
-
-
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
@@ -31,8 +27,20 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,
                               default='draft')
 
+    objects = models.Manager()  # Менеджер по умолчанию.
+    published = PublishedManager()  # Наш новый менеджер.
+
+    # С помощью следующей команды мы получим все опубликованные статьи,
+    # название которых начинается с Who:
+    # Post.published.filter(title__startswith='Who')
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day, self.slug])
