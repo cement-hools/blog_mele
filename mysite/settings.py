16fd09485cbde233f023410f74cd492daaf3da09
Максим Секачёв <cement-fan@ya.ru>
2021-01-05 18:22:00 +0300

    SiteMap

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -39,8 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog.apps.BlogConfig',
-    'taggit',
+    'taggit', # теги
+    'django.contrib.sites',    # карта сайта
+    'django.contrib.sitemaps', # карта сайта
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
