a0461d8988ef6d18cda2bd2f06ed3f2442faa13e
Максим Секачёв <cement-fan@ya.ru>
2021-01-06 05:14:13 +0300

    search

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
 
 
@@ -99,3 +99,19 @@ def post_detail(request, year, month, day, post):
                       'similar_posts': similar_posts,
                   }
                   )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        results = Post.objects.filter(
+            Q(title__icontains=query) | Q(body__icontains=query)
+            )
+    return render(request, 'blog/post/search.html', {'form': form,
+                                                     'query': query,
+                                                     'results': results})
